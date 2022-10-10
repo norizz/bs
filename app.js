157796ad94206f15df17a20e0f953badb58db0d2
norizz <113930040+norizz@users.noreply.github.com>
2022-10-10 18:17:27 +0600

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector("#burger-img");
-const menu = document.querySelector("#menu");
-
-burger.addEventListener("click" , () => {
-  menu.classList.toggle("disp");
-});
